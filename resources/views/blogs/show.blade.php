02c11bf34189674a60b035a607989a27f41413e3
David <david@suuty.com>
2018-01-26 15:06:34 -0800

    fixing blog

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -9,12 +9,12 @@ $image = $decodedarr[0];
 <div class="container">
   <strong><h1>{{$blog->title}}</h1></strong>
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-8 text-center">
       <img src="{{ asset('/../images/') }}/{{$image}}" style="border: solid 0px #000000; width: 100%;"/>
     </div>
   </div>
   <div class="row">
-    <div class="col-md-8 text-center">
+    <div class="col-md-8">
       <div class ="panel panel-default">
         <div class="panel-heading">
           <?php
