12c2a34e9077bdf5e0a82db8004072777b1545e1
David <david@suuty.com>
2018-01-26 15:02:19 -0800

    fixing blog img

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -52,7 +52,7 @@ class BlogController extends Controller
                 $filename = $file->getClientOriginalName();
                 $extension = $file->getClientOriginalExtension();
                 $picture = $filename;
-                $destinationPath = base_path() . '\public\images';
+                $destinationPath = public_path('images');
                 array_push( $imagepaths, $filename);
                 $file->move($destinationPath, $picture);
             }
