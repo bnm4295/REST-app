7035374f977dab8cf9a891c63d7d159c85d91ceb
David <david@suuty.com>
2018-01-29 12:43:31 -0800

    added verified to fb login

--- app/User.php
+++ app/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'profileimg', 'phone', 'aboutme',
+        'name', 'email', 'password', 'profileimg', 'phone', 'aboutme', 'verified',
     ];
 
     /**
