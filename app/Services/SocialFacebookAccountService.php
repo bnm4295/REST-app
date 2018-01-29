c067a50bbe945183a56f700ba6ef2eec98123a89
David <david@suuty.com>
2018-01-29 12:19:30 -0800

    added verified to fb login

--- app/Services/SocialFacebookAccountService.php
+++ app/Services/SocialFacebookAccountService.php
@@ -30,6 +30,7 @@ class SocialFacebookAccountService
                     'email' => $providerUser->getEmail(),
                     'name' => $providerUser->getName(),
                     'password' => md5(rand(1,10000)),
+                    'verified' => 1,
                 ]);
             }
 
