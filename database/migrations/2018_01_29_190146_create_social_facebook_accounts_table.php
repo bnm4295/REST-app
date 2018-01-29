c67c1a2dfed170f61e7ae503b5e7215eda8728bc
David <david@suuty.com>
2018-01-29 12:11:40 -0800

    added primary key

--- database/migrations/2018_01_29_190146_create_social_facebook_accounts_table.php
+++ database/migrations/2018_01_29_190146_create_social_facebook_accounts_table.php
@@ -14,10 +14,11 @@ class CreateSocialFacebookAccountsTable extends Migration
     public function up()
     {
         Schema::create('social_facebook_accounts', function (Blueprint $table) {
-          $table->integer('user_id');
-          $table->string('provider_user_id');
-          $table->string('provider');
-          $table->timestamps();
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('provider_user_id');
+            $table->string('provider');
+            $table->timestamps();
         });
     }
 
