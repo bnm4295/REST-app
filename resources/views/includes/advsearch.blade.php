025aa34a6906e0251dbedbbf0c55e9f9023013d9
David <david@suuty.com>
2018-01-30 11:17:44 -0800

    some styles to adv search

--- resources/views/includes/advsearch.blade.php
+++ resources/views/includes/advsearch.blade.php
@@ -1,76 +1,80 @@
-<div id="filter-panel" class="filter-panel">
+<div id="filter-panel" class="filter-panel" style="text-align: initial;">
   <div class="panel panel-default">
       <div class="panel-body">
-        <!-- {{url('properties')}} -->
           <form action="{{url('/properties')}}" method="GET" class="form-inline" role="form">
-
-              <div class="form-group">
-                  <label class="filter-col" style="margin-right:0;" for="numbeds">Beds:</label>
-                  <select id="numbeds"class="form-control" name="number_of_beds">
-                     <option value=""></option>
-                     <option value="1">1</option>
-                     <option value="2">2</option>
-                     <option value="3">3</option>
-                     <option value="4">4</option>
-                     <option value="5">5</option>
-                </select>
-              </div> <!-- form group [rows] -->
-              <div class="form-group">
-                  <label class="filter-col" style="margin-right:0;" for="numbaths">Baths:</label>
-                  <select id="numbaths"class="form-control" name="number_of_baths">
-                      <option value=""></option>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
+            <div style="line-height: 3; text-align: center;">
+                <div class="form-group">
+                    <label class="filter-col" style="margin-right:0;" for="numbeds">Beds:</label>
+                    <select id="numbeds"class="form-control" name="number_of_beds">
+                       <option value=""></option>
+                       <option value="1">1</option>
+                       <option value="2">2</option>
+                       <option value="3">3</option>
+                       <option value="4">4</option>
+                       <option value="5">5</option>
                   </select>
-              </div> <!-- form group [rows] -->
-              <div class="form-group">
-                  <label class="filter-col" style="margin-right:0;" for="proptype">Property Type:</label>
-                  <select id="proptype" class="form-control" name="house_type">
-                    <option value="Single Family Home">Single Family Home</option>
-                    <option value="Apartment">Apartment</option>
-                    <option value="Condo">Condo</option>
-                  </select>
-              </div> <!-- form group [rows] -->
-              <div class="form-group">
+                </div>
+                <div class="form-group">
+                    <label class="filter-col" style="margin-right:0;" for="numbaths">Baths:</label>
+                    <select id="numbaths"class="form-control" name="number_of_baths">
+                        <option value=""></option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label class="filter-col" style="margin-right:0;" for="proptype">Property Type:</label>
+                    <select id="proptype" class="form-control" name="house_type">
+                      <option value="Single Family Home">Single Family Home</option>
+                      <option value="Apartment">Apartment</option>
+                      <option value="Condo">Condo</option>
+                    </select>
+                </div>
+                <div class="form-group">
                   <label class="filter-col" style="margin-right:0;" for="pref-search">City:</label>
                   <input type="text" class="form-control input-sm" id="pref-search" name="addr">
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <h4>Price Range:</h4>
               </div>
-              <!-- form group [search] -->
-              <!--div class="form-group">
-                  <label class="filter-col" style="margin-right:0;" for="pref-orderby">Order by:</label>
-                  <select id="pref-orderby" class="form-control">
-                      <option>Descendent</option>
-                  </select>
-              </div>  form group [order by] -->
-              <h2>Price Range</h2>
-              <div class="col-sm-6">
+              <div class="col-sm-4" style="margin-right: -30px;">
                   <div class="range-slider">
                     <input class="range-slider__range" type="range" value="0" min="0" max="5000000" name="price_left">
                     <span class="range-slider__value">0</span>
                   </div>
               </div>
-              <div class="col-sm-6">
+              <div class="col-sm-4">
                   <div class="range-slider">
                     <input class="range-slider__range" type="range" value="2500000" min="2500000" max="5000000" name="price_right">
                     <span class="range-slider__value">0</span>
                   </div>
               </div>
-              <h2>Area Range</h2>
+            </div>
+            <div class="row">
               <div class="col-sm-3">
-                  <div class="range-slider" style="margin-right: -20px;">
+                <h4>Area Range:</h4>
+              </div>
+              <div class="col-sm-4" style="margin-right: -30px;">
+                  <div class="range-slider">
                     <input class="range-slider__range" type="range" value="0" min="0" max="50000" name="area_left">
                     <span class="range-slider__value">0</span>
                   </div>
               </div>
-              <div class="col-sm-3">
+              <div class="col-sm-4">
                   <div class="range-slider">
                     <input class="range-slider__range" type="range" value="25000" min="25000" max="50000" name="area_right">
                     <span class="range-slider__value">0</span>
                   </div>
               </div>
+            </div>
+            <hr>
+            <div class="row" style="text-align: center;">
               <div class="form-group">
                 <div class="checkbox" style="margin-left:10px; margin-right:10px;">
                   <label><input type="checkbox" name="mempar">Save Search</label>
@@ -79,6 +83,7 @@
               <button type="submit" class="btn btn-default filter-col">
                 <i class="fa fa-search" aria-hidden="true"></i> Search
               </button>
+            </div>
           </form>
       </div>
   </div>
