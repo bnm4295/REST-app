250d43f445faba3b4638b371f6b08b944d34dd11
David <david@suuty.com>
2018-02-13 14:43:13 -0800

    changing blog layout

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -26,31 +26,29 @@
 </div>-->
 
 <div class="container">
-  <h3 style="text-align: center">Blog Posts</h3>
+  <div class="row">
+    <h3 style="text-align: center">Blog Posts</h3>
+  </div>
     @foreach($blogs as $blog)
-    <div class="row">
-      <?php
-      $decodedarr = json_decode( $blog->images , true);
-      $counter = count($decodedarr);
-      $image = $decodedarr[0];
-      ?>
-      <div class="col-md-4 text-center">
-        <div class ="panel panel-default">
-          <a href="{{ url('blogs')}}/{{$blog->id}}"><input class="img-rounded" value="" type="submit" style="border: solid 0px #000000; height: 200px; width: 100%;
-            background-image: url({{ asset('/../images/') }}/{{$image}});
-            background-size: 375px; background-repeat: no-repeat;"/></a>
-          <div class="panel-heading">
-            <div style="text-align: left;">
-              <strong>{{$blog->title}}</strong>
-            </div>
+    <?php
+    $decodedarr = json_decode( $blog->images , true);
+    $counter = count($decodedarr);
+    $image = $decodedarr[0];
+    ?>
+    <div class="col-md-4 text-center">
+      <div class ="panel panel-default">
+        <a href="{{ url('blogs')}}/{{$blog->id}}"><input class="img-rounded" value="" type="submit" style="border: solid 0px #000000; height: 200px; width: 100%;
+          background-image: url({{ asset('/../images/') }}/{{$image}});
+          background-size: 375px; background-repeat: no-repeat;"/></a>
+        <div class="panel-heading">
+          <div style="text-align: left;">
+            <strong>{{$blog->title}}</strong>
           </div>
         </div>
       </div>
     </div>
+    @endforeach
 </div>
-@endforeach
-</div>
-
 
 @endsection('content')
 @section('footer')
