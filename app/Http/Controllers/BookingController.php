1db90dd4ab39922192d38d5b133a368923b4bf52
David <david@suuty.com>
2018-02-14 14:22:33 -0800

    html email complete

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,6 +4,9 @@ namespace Suuty\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Suuty\Booking;
+use Suuty\Property;
+use Suuty\User;
+use Mail;
 use Session;
 
 class BookingController extends Controller
@@ -21,6 +24,22 @@ class BookingController extends Controller
       ]);
       $inputs = $request->all();
       $offers = Booking::Create($inputs);
+      $from = $request->email;
+      $prop_id = $inputs['prop_id'];
+      $property = Property::find($prop_id);
+      $user_id = $property->user_id;
+      $user = User::find($user_id);
+      $useremail = $user->email;
+      $messages =
+        "<br>Comments: ".$request->description . "<br>First Date: ".$request->openfirst.
+        "<br>Second Date: ".$request->opensecond.
+        "<br>";
+      Mail::send(['html' =>'emails.booking'], ['text' => $messages], function($message) use ($from,$useremail)
+      {
+        $message->subject('Suuty - Requested Viewing Time');
+        $message->from($from, 'Suuty');
+        $message->to($useremail);
+      });
       Session::flash('success', 'Request Successful!');
       return redirect()->back();
     }
