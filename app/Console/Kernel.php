e1699771bb94e98d4238ab872a3293ac2d9841cd
David <david@suuty.com>
2018-03-20 15:24:11 -0700

    fixing search

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -72,7 +72,13 @@ class Kernel extends ConsoleKernel
               ->get();
 
               foreach($savesearch as $post){
-                $messages .= "<a href='https://www.suuty/$post->url'>SavedSearch</a>";
+                $messages .= "<a href='https://suuty.com$post->url'>
+                City: $post->addr |
+                PriceMIN: $post->price_left | PriceMAX: $$post->price_right |
+                AreaMIN: $post->area_left sqft | AreaMAX: $post->area_right sqft
+                  Beds: $post->number_of_beds
+                  Baths: $post->number_of_baths
+                </a>""<br>";
               }
 
               foreach($savesearch as $post){
